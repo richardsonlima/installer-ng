dfa8c91bf689b3298fbd9f753e5cd203e2e804e9
Thomas Orozco <thomas@orozco.fr>
2015-01-09 18:05:27 +0100

    Fix for real

--- config/software/libaio.rb
+++ config/software/libaio.rb
@@ -6,11 +6,12 @@ default_version '0.3.110'
 source url: "http://ftp.debian.org/debian/pool/main/liba/libaio/libaio_#{version}.orig.tar.gz",
        md5: '2a35602e43778383e2f4907a4ca39ab8'
 
+relative_path "libaio-#{version}"
+
 build do
   env = with_standard_compiler_flags(with_embedded_path)
 
-  make "-j #{workers} prefix=#{install_dir}/embedded", env: env
-  make "-j #{workers} install", env: env
+  make "-j #{workers} prefix=#{install_dir}/embedded install", env: env
 
   # TODO - Remove libredhat-kernel.so??
 end
