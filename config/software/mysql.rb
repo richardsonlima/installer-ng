4f51481101989091dba2329cae52b0fe80f5c387
Thomas Orozco <thomas@orozco.fr>
2015-01-09 16:31:21 +0100

    Add MySQL software?

---
+++ config/software/mysql.rb
@@ -0,0 +1,21 @@
+
+name 'mysql-server'
+default_version '5.6.22'
+
+source  :url => "http://dev.mysql.com/get/Downloads/MySQL-5.6/mysql-#{version}.tar.gz",
+        :md5 => '3985b634294482363f3d87e0d67f2262'
+
+relative_path "mysql-#{version}"
+
+
+# TODO - Check --enable-languages (mysqlbug)
+build do
+  env = with_standard_compiler_flags(with_embedded_path)
+
+  command "./configure" \
+          " --prefix=#{install_dir}/embedded" \
+          " --exec-prefix=#{install_dir}/embedded", env: env
+
+  make "-j #{workers}", env: env
+  make "-j #{workers} install", env: env
+end
