11e0c3568e4f644a110fd9a4891524a7bb4fff4f
Thomas Orozco <thomas@orozco.fr>
2015-01-09 13:28:36 +0100

    Initial new omnibus project structure

---
+++ config/projects/test.rb
@@ -0,0 +1,28 @@
+#
+# Copyright 2015 YOUR NAME
+#
+# All Rights Reserved.
+#
+
+name "test"
+maintainer "CHANGE ME"
+homepage "https://CHANGE-ME.com"
+
+# Defaults to C:/test on Windows
+# and /opt/test on all other platforms
+install_dir "#{default_root}/#{name}"
+
+build_version Omnibus::BuildVersion.semver
+build_iteration 1
+
+# Creates required build directories
+dependency "preparation"
+
+# test dependencies/components
+# dependency "somedep"
+
+# Version manifest file
+dependency "version-manifest"
+
+exclude "**/.git"
+exclude "**/bundler/git"
