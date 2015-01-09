c6d4bb9bd9b60a3eef16de501fc6e894c7ef62b7
Thomas Orozco <thomas@orozco.fr>
2015-01-09 18:37:50 +0100

    Add php, chef-gem

--- config/projects/scalr-server.rb
+++ config/projects/scalr-server.rb
@@ -7,15 +7,15 @@ install_dir "#{default_root}/#{name}"
 build_version Omnibus::BuildVersion.semver
 build_iteration 1
 
-dependency 'chef-gem' # for embedded chef-solo
-
 # Creates required build directories
 dependency 'local-preparation'
 
 # Software we need to run
 dependency 'mysql'
+dependency 'php'
 
 # Actual Scalr software
+dependency 'chef-gem' # for embedded chef-solo
 dependency 'scalr-server-cookbooks'   # Cookbooks to configure Scalr
 dependency 'scalr-server-ctl'         # CLI to run chef-solo and actions (scalr-server-ctl)
 
