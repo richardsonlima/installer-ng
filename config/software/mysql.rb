68e108bfd46d0ee8a1681d21767330b4055c3428
Thomas Orozco <thomas@orozco.fr>
2015-01-09 17:02:27 +0100

    Fix mysql software

--- config/software/mysql.rb
+++ config/software/mysql.rb
@@ -17,8 +17,6 @@ relative_path "mysql-#{version}"
 build do
   env = with_standard_compiler_flags(with_embedded_path)
 
-  command 'cmake -DBUILD_CONFIG=mysql_release .', env: env
-
   command [
               'cmake',
               # General flags
@@ -30,14 +28,14 @@ build do
               '-DWITH_SSL=system',
               "-DOPENSSL_INCLUDE_DIR:PATH=#{install_dir}/embedded/include",
               "-DOPENSSL_LIBRARIES:FILEPATH=#{install_dir}/embedded/lib/libssl.so",
-              "-DWITH_ZLIB=system",
+              '-DWITH_ZLIB=system',
               "-DZLIB_INCLUDE_DIR:PATH=#{install_dir}/embedded/include",
               "-DZLIB_LIBRARY:FILEPATH=#{install_dir}/embedded/lib/libz.so",
               "-DCRYPTO_LIBRARY:FILEPATH=#{install_dir}/embedded/lib/libcrypto.so",
               # Feature flags
               '-DDEFAULT_CHARSET=utf8',
               '-DDEFAULT_COLLATION=utf8_unicode_ci',
-              ".",
+              '.',
           ].join(' '), :env => env
 
   make "-j #{workers}", env: env
