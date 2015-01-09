c5cdb09d62d76f655a6756e0fdc44cd79d182423
Thomas Orozco <thomas@orozco.fr>
2015-01-09 16:50:19 +0100

    More MySQL deps

--- config/software/mysql.rb
+++ config/software/mysql.rb
@@ -2,6 +2,11 @@
 name 'mysql-server'
 default_version '5.6.22'
 
+dependency 'openssl'
+dependency 'zlib'
+dependency 'ncurses'
+
+
 source  :url => "http://dev.mysql.com/get/Downloads/MySQL-5.6/mysql-#{version}.tar.gz",
         :md5 => '3985b634294482363f3d87e0d67f2262'
 
@@ -11,6 +16,8 @@ relative_path "mysql-#{version}"
 # TODO - Check --enable-languages (mysqlbug)
 build do
   env = with_standard_compiler_flags(with_embedded_path)
+
+  command 'cmake -DBUILD_CONFIG=mysql_release .', env: env
 
   command "./configure" \
           " --prefix=#{install_dir}/embedded" \
